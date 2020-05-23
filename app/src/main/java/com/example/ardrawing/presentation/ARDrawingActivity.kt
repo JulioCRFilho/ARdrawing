8c62c8b672b9789c7e9b6696ed0cd5f9c405650e
Julio Cesar da Costa Reis Filho <juliofilho@brq.com>
2020-05-23 20:49:49 -0300

    arcore funcionou

--- app/src/main/java/com/example/ardrawing/presentation/ARDrawingActivity.kt
+++ app/src/main/java/com/example/ardrawing/presentation/ARDrawingActivity.kt
@@ -11,13 +11,13 @@ import com.example.ardrawing.viewModel.ARDrawingViewModel
 import com.google.ar.core.ArCoreApk
 import com.google.ar.core.Session
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
-import com.google.ar.sceneform.ArSceneView
+import com.google.ar.sceneform.ux.ArFragment
 
 class ARDrawingActivity : AppCompatActivity() {
     val viewModel: ARDrawingViewModel by viewModels()
     private lateinit var cameraPermissionHelper: CameraPermissionHelper
-    lateinit var mSession: Session
-    lateinit var arSceneView: ArSceneView
+    lateinit var arFragment: ArFragment
+    var mSession: Session? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,26 +27,38 @@ class ARDrawingActivity : AppCompatActivity() {
     }
 
     private fun initVars() {
-        arSceneView = ArSceneView(this).findViewById(R.id.arFragmentView)
-        mSession = Session(this)
         cameraPermissionHelper = CameraPermissionHelper()
+    }
+
+    private fun initArCore() {
+        if (cameraPermissionHelper.hasCameraPermission(this)) {
+            arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
+            mSession = Session(this)
+        } else {
+            requestPermission()
+        }
     }
 
     override fun onResume() {
         super.onResume()
+        verifyArCoreAPK()
+    }
+
+    private fun verifyArCoreAPK() {
         try {
             if (mSession == null) {
                 when (ArCoreApk.getInstance().requestInstall(this, true)) {
-                    ArCoreApk.InstallStatus.INSTALLED -> mSession = Session(this)
-                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> ArCoreApk.getInstance().requestInstall(this, true)
-                    null -> return
+                    ArCoreApk.InstallStatus.INSTALLED -> initArCore()
+                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> ArCoreApk.getInstance()
+                        .requestInstall(this, true)
+                    null -> onResume()
                 }
             }
         } catch (e: UnavailableUserDeclinedInstallationException) {
-            Toast.makeText(this, "TODO: handle exception $e", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
             return
         } catch (e: Exception) {
-            Toast.makeText(this, "TODO: handle exception ${e.message}", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
             return
         }
     }
@@ -61,11 +73,17 @@ class ARDrawingActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (!cameraPermissionHelper.hasCameraPermission(this)) {
-            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
-                    .show()
+            Toast.makeText(
+                this,
+                "Camera permission is needed to run this application",
+                Toast.LENGTH_LONG
+            )
+                .show()
             if (!cameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
                 cameraPermissionHelper.launchPermissionSettings(this);
             }
+        } else {
+            verifyArCoreAPK()
         }
     }
 }
